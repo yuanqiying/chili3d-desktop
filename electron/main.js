d5d975d3d791db0d075633a933ed4e02ba54c6c0
yuanqiying <57630303+yuanqiying@users.noreply.github.com>
2026-06-13 12:37:08 +0800

    Update main.js

--- electron/main.js
+++ electron/main.js
@@ -1,1 +1,17 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  win.loadFile('dist/index.html');
+}
+
+app.whenReady().then(createWindow);
 
